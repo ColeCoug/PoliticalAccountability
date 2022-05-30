85f9bf00fd1a8193c0e7c3290fc4417a05c0f8a6
ColeCoug <40366361+ColeCoug@users.noreply.github.com>
2022-05-30 13:56:32 -0500

    Added sign in and sing up
    Sign in and sign up added, they share a common sign.css and have their own css files for the input boxes.

---
+++ signup.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Sign up page for PA account">
+    <meta name="author" content="Cole Coughlin">
+    <title>Sign up | Political Accountability</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+   
+
+    
+    <!-- Custom styles for this template -->
+    <link href="sign.css" rel="stylesheet">
+    <link href="signup.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+    
+<main class="form-signup w-100 m-auto">
+  <form>
+    <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Create a Political Accountablity account</h1>
+
+    <div class="form-floating">
+      <input type="first-name" class="form-control" id="floatingInput" placeholder="First name">
+      <label for="floatingInput">First name</label>
+    </div>
+    <div class="form-floating">
+      <input type="last-name" class="form-control" id="floatingInput" placeholder="Last name">
+      <label for="floatingInput">Last name</label>
+    </div>
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingInput" placeholder="Password">
+      <label for="floatingInput">Password</label>
+    </div>
+    <div class="form-floating">
+      <input type="retype-password" class="form-control" id="floatingPassword" placeholder="Retype Password">
+      <label for="floatingPassword">Retype Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Email me weekly updates
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+    <p class="mt-5 mb-3 text-muted">&copy; 2022</p>
+  </form>
+</main>
+
+
+    
+  </body>
+</html>
